4b4d2aba69a69d4393e77e81ac8ff13cf9009b42
Alex Jablonski <alexmjablonski@gmail.com>
2014-07-16 01:50:33 -0500

    Add and pass first AccountTest ( shouldIncreaseMyBalanceWhenIDepositMoney() )

---
+++ src/main/java/com/thoughtworks/accountbalance/Account.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.accountbalance;
+
+public class Account {
+    private double balance;
+
+    public Account(double startingBalance) {
+        balance = startingBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
--- src/test/java/com/thoughtworks/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/accountbalance/AccountTests.java
@@ -1,12 +1,17 @@
 package com.thoughtworks.accountbalance;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AccountTests {
-//    @Test
-//    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-//
-//    }
+    @Test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+        Account a = new Account(100);
+
+        a.deposit(50);
+
+        assertEquals(150, a.getBalance(), 0.001);
+    }
 //
 //    @Test
 //    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
